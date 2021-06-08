68ebf29517a9b7e26cb1bcb7f177b83dfb11ec91
Fernando Issao Kassa <fernandokassa@yahoo.com.br>
2021-06-08 08:54:52 -0300

    Have learned how to reference DLL in ByteBank.SistemaInterno

---
+++ ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,20 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(123, 135135);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
