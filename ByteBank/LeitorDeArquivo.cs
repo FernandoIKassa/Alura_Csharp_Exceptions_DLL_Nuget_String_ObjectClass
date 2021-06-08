0f2686212f1bbc2fcd0b6c5d7ad7e767ce531585
Fernando Issao Kassa <36009834+FernandoIKassa@users.noreply.github.com>
2021-06-07 22:06:02 -0300

    Revert "Initial commit - handling libraries"

---
+++ ByteBank/LeitorDeArquivo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBank
+{
+    class LeitorDeArquivo : IDisposable
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+
+            //throw new FileNotFoundException();
+
+            Console.WriteLine("Abrindo o arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+
+            //throw new IOException();
+
+            return "Linha do arquivo";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando o arquivo...");
+        }
+
+    }
+}
---
+++ ByteBank/OperacaoFinanceiraException.cs
@@ -0,0 +1,24 @@
+﻿// using _05_ByteBank;
+
+using System;
+using System.Runtime.Serialization;
+
+namespace ByteBank
+{
+    public class OperacaoFinanceiraException : Exception
+    {
+        public OperacaoFinanceiraException()
+        {
+        }
+
+        public OperacaoFinanceiraException(string message) : base(message)
+        {
+        }
+
+        public OperacaoFinanceiraException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        
+    }
+}
