8613840384a0ca10dacffb08e217157b7b5405bd
Fernando Issao Kassa <fernandokassa@yahoo.com.br>
2021-07-15 22:44:18 -0300

    Use of String methods (IndexOf, IsNullOrEmpty, Remove, ToUpper,ToLower etc) and use of a basic regular expression with REGEX

---
+++ ByteBank/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class ExtratorValorDeArgumentosURL
+    {
+        public string URL { get; }
+        private readonly string _argumentos;
+
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if (String.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio", nameof(url));
+            }
+
+            URL = url;
+            int indiceInterrogacao = url.IndexOf("?");
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+        }
+
+        public string GetValor(string nomeParametro)
+        {
+            nomeParametro = nomeParametro.ToUpper();
+            string argumentoCaixaAlta = _argumentos.ToUpper();
+            
+            string termo = nomeParametro + "=";
+            int indiceTermo = argumentoCaixaAlta.IndexOf(termo);
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if (indiceEComercial == -1)
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);
+        }
+    }
+}
--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Humanizer;
+using System.Text.RegularExpressions;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,14 +13,38 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string url = "pagina?argumentos";
-            string argumentos = url.Substring(7);
+            //string url = "pagina?argumentos";
+            //int indiceInterrogacao = url.IndexOf("?");
+            //Console.WriteLine(indiceInterrogacao);
+            //
+            //string argumentos = url.Substring(indiceInterrogacao + 1);
+            //
+            //Console.WriteLine(url);
+            //Console.WriteLine(argumentos);
 
-            Console.WriteLine(url);
-            Console.WriteLine(argumentos);
+            //string url = "www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
+            //Console.WriteLine(extrator.GetValor("moedaOrigem")); // real
+            //Console.WriteLine(extrator.GetValor("moedaDestino")); // dolar
+            //Console.WriteLine(extrator.GetValor("valor")); // 1500
 
-            
+            //REGEX - another examples
+            //string patternPhone = "[0123456789][0123456789][0123456789][0123456789][-])[0123456789][0123456789][0123456789][0123456789]"
+            //string patternPhone = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            //string patternPhone = "[0-9]{4}[-][0-9]{4}";
+            //string patternPhoneCellPhone = "[0-9]{4,5}[-][0-9]{4,5}";
+            //string patternPhoneCellPhone = "[0-9]{4,5}-[0,1][0-9]{4,5}";
+            //string patternPhoneCellPhone = "[0-9]{4,5}-?[0-9]{4,5}";
+
+            string padraoNumeroTelefoneCelular = "[0-9]{4,5}-?[0-9]{4,5}";
+            string texto = "Meu numero de celular é 91234-1234.";
+
+            Match match = Regex.Match(texto, padraoNumeroTelefoneCelular);
+            Console.WriteLine(match.Value);
+
             Console.ReadLine();
         }
+
+        
     }
 }
